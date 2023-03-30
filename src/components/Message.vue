de808daaa8f2185586bc59ad3fa6f3905b16e7d3
Lisa <lisa06262@gmail.com>
2023-03-30 22:21:34 +0300

    added styles, component

---
+++ src/components/Message.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="message">
+        <div class="user">
+            <p class="name"> Samuel Jackson </p>
+            <p class="date"> 13 Apr 2022 </p>
+        </div>
+        <div class="message-form">
+            <div class="triangle"></div>
+            <div class="content">
+               <p>Hey Eva! You're cool. Nice pic!</p> 
+            </div>
+        </div>
+        <div class="send-message">
+            <textarea></textarea>
+            <button>  <p>Send a message  </p> </button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MessageVue'
+}
+</script>
+
+
+<style scoped lang="scss">
+
+.message {
+    .user {
+        display: flex;
+        align-items: center;
+
+        .name {
+            font-weight: 700;
+            font-size: 14px;
+            margin-right: 11px;
+        }
+        .date {
+            font-size: 11px;
+            color:#808080 ;
+            
+        }
+    }
+    .message-form {
+        position: relative;
+        margin-bottom: 10px;
+        height: 50px;
+        border: 1px solid #C4CBCF;
+        padding: 0% 3% 0 3%;
+    }
+     .content {
+        display: flex;
+        align-items: center;
+     }
+    .send-message {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        bottom: 0;
+        width: 93%;
+        position: absolute;
+        height: 24%;
+
+        textarea {
+            width: 100%;
+            height: 4rem;
+        }
+
+        button {
+            height: 3rem;
+            background-color: #FDD639;
+            border: none;
+            border-radius: 23px;
+            width: 45%;
+            margin-top: 4%;
+            font-size: 16px;
+            font-weight: 700;
+
+        }
+        
+    }
+    .triangle {
+        position: absolute;
+        top: -15px;
+        left: 15px;
+        width: 0;
+        height: 0;
+        border-top: 0;
+        border-right: 12px solid transparent;
+        border-bottom: 15px solid #C4CBCF;
+    }
+}
+
+</style>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -7,22 +7,93 @@
             <p class="name"> Eva Jonson </p>
             <p class="prof"> Sales Manager </p>
             <div>
-              I will find the best offers for you. <br>
-              My services are absolutely free.
+              <p>
+                I will find the best offers for you. <br>
+                My services are absolutely free.
+              </p>
             </div>
           </div>
+        </div>
+
+        <div class="chart">
+          <div class="wrapper">
+            <p class="gray"> Services </p>
+            <hr>
+            <div class="items-box">
+                <div class="item">
+                  <div class="booking general"> 
+                     <p class="options"> 
+                        Manual tour booking
+                     </p> 
+                  </div>
+                  <div class="count-box">
+                    <p class="count"> 11 </p>
+                  </div>
+                </div>
+                <div class="item">
+                  <div class="tours general"> 
+                    <p class="options">
+                       Package tours 
+                    </p>
+                  </div>
+                  <div class="count-box">
+                    <p class="count"> 3 </p>
+                  </div>
+                </div>
+                <div class="item">
+                  <div class="hotels general">
+                     <p class="options"> 
+                      Hotels 
+                    </p>
+                  </div>
+                  <div class="count-box">
+                    <p class="count"> 1 </p>
+                  </div>
+                </div> 
+                <hr>
+                <div class="item">
+                  <div>
+                    <p class="options total"> Total </p>
+                  </div>
+                  <div class="count-box">
+                     <p class="total"> 15 </p>
+                  </div>
+                </div>
+            </div>
+          </div>
+        </div>
+        <div class="messages-wrapper">
+            <div class="reviews">
+              <div>
+                  <p> Latest reviews </p>
+                  <a href="#"> All reviews </a>
+              </div>
+              <div class="icons">
+                <div  >
+                  <img src="../assets/like.svg" alt="">
+                  <p>131</p>
+                </div>
+                <div>
+                  <img  src="../assets/comments.svg" alt="">
+                  <p>14</p>
+                </div>
+              </div>
+            </div>
+              <Message />
         </div>
       </div>
     </div>
   </template>
   
   <script>
+import Message from '@/components/Message.vue';
+export default {
+      components: { Message }
+}
 
-  
- 
-  
   </script> 
-  <style lang="scss">
+
+  <style lang="scss" scoped>
 
   .container {
     display: flex;
@@ -30,37 +101,161 @@
     align-items: center;
     height: 100vh;
 
-    .message-block {
-        height: 55rem;
-        width: 30%;
+    .message-block { 
+        height: 90vh;
+        width: 80%;
         background-color: white;
-        padding: 1%;
+        padding: 3%;
+        position: relative;
+        }
+        .messages-wrapper {
+          height: min-content;
 
+            .reviews {
+              display: flex;
+              justify-content: space-between;
+
+            .icons {
+                display: flex;
+                width: 20%;
+                justify-content: space-around;
+
+                img {
+                  margin-right: 5px;
+                }
+            }
+              div {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                
+               
+                a {
+                  margin-left: 10px;
+                  color: #005DA1;
+                }
+              }
+            }
+        }
+        .chart {
+          height: min-content;
+
+          display: flex;
+          flex-direction: row-reverse;
+          width: 50%;
+          margin-left: 50%;
+
+          .gray {
+            font-size: 13px ;
+            color: #333333;
+            margin-left: 75%;
+          }
+          .wrapper {
+            width: 100%;
+          }
+
+          .items-box {
+            
+            .item {
+              height: 2rem;
+              margin-top: 1%;
+              display: flex;
+              align-items: center;
+              justify-content: space-between;
+              width: 100%;
+              
+
+              .count {
+                font-weight: 700;
+              }
+                
+              p  {
+                font-size: 13px;
+                font-weight: 400;
+                color: #333333;
+                white-space: nowrap;
+              }
+              .options {
+                padding-left: 8px;
+              }
+
+              .total {
+                font-weight: 700;
+                font-size: 16px;
+              }
+              .count-box {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                width: 32%;
+              }
+
+              .general {
+                height: 100%;
+                border-radius:  0 5px 5px 0;
+                display: flex;
+                align-items: center;
+
+              }
+              .booking {
+                background-color:#B1E19B;
+                width: 56%;
+              }
+
+              .tours {
+                background-color:#ACE2F8 ;
+                width: 18%;
+              }
+
+              .hotels {
+                background-color:#ACE2F8 ;
+                width: 12%;
+              } 
+            }
+          }
+        }
         .header {
             display: flex;
 
             img {
                 border-radius: 50%;
+                height:10rem;
+                z-index: 2;
             }
-
+           
             .profile-info {
                 margin-left: 2%;
+                width: 100%;
 
                 div {
-                    border: 1px solid #DADADA ;
-                    background-color: #FFFBC8;
-                    height: 3rem;
-                    display: flex;
-                    align-items: center;
-                    width: 150%;
+                  border: 1px solid #DADADA;
+                  background-color: #FFFBC8;
+                  display: flex;
+                  align-items: center;
+                  width: 115%;
+                  margin-left: -60px;
+                  
+
+                    p {
+                      font-size: 14px;
+                      font-weight: 400;
+                      padding-left: 14%;
+                      line-height: 20px;
+                    }
                 }
 
                 .name {
-                    font-size: $size;
+                    font-size: 16px;
+                    font-weight: 700;
+                    color: #333333;
+                }
+                .prof {
+                  font-size: 12px;
+                  font-weight: 400;
+                  color: #808080;
                 }
             }
         }
-
     }
-  }
+
   </style>
